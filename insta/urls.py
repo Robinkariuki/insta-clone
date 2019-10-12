c4e7f0cf660c96efe9730c8c6119ca4a314d2086
Robin <robinkaris7@gmail.com>
2019-10-13 01:50:08 +0300

    this commit makes changes to views.py and also adds forms.py

--- insta/urls.py
+++ insta/urls.py
@@ -7,6 +7,7 @@ from django.conf.urls.static import static
 urlpatterns=[
     url(r'^$',views.timeline,name='Timeline'),
     
+    
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- insta/views.py
+++ insta/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from .models import Post
 # Create your views here.
 @login_required(login_url='/accounts/login/')
 def timeline(request):
     
-   
+    posts = Post.objects.all()
+    current_user = request.user
+    profiles = Profile.objects.all()
+    form = CommentForm()
+    comments = Comment.objects.all()
 
-    return render(request,'timeline.html')
+    return render(request,'timeline.html',{ "posts": posts ,"form":form, "comments":comments,"profiles":profiles})
