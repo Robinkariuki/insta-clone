4e5f66b5b54d5df61339b4732af1c691a42a0073
Robin <robinkaris7@gmail.com>
2019-10-13 01:52:01 +0300

    this commit adds a profileform

--- insta/forms.py
+++ insta/forms.py
@@ -13,3 +13,8 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         exclude=['username','post_date','likes','p_pic']
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['profile_pic','bio','email']
