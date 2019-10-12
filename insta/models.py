fe1c5a05d889da9b8de0efcacab8de30f1b8adf7
Robin <robinkaris7@gmail.com>
2019-10-13 01:59:40 +0300

    this commit adds a profile model

--- insta/models.py
+++ insta/models.py
@@ -1,5 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
+from tinymce.models import HTMLField
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+
+
+Gender=(
+    ('Male','Male'),
+    ('Female','Female'),
+)
 # Create your models here.
 class Post(models.Model):
     
@@ -23,3 +32,29 @@ class Post(models.Model):
 
     def __str__(self):
         return self.image_name
+
+class Profile(models.Model):
+    profile_pic = models.ImageField(upload_to='profilepics/', default = 'profilepics/default.jpg')
+    bio = HTMLField()
+    user = models.OneToOneField(User,on_delete=models.CASCADE,)
+    
+    email= models.EmailField()
+    gender = models.CharField(max_length=15,choices=Gender,default="Male")
+
+
+    @classmethod
+    def search_profile(cls,search_term):
+        profiles = cls.objects.filter(user__username__icontains=search_term)
+        return profiles
+
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.profile.save()
+
+    def __str__(self):
+        return self.user.username
