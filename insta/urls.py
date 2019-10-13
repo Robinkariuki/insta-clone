e3215bd5bb5b8b99e897d6717c5004c6b59eb0ad
Robin <robinkaris7@gmail.com>
2019-10-13 08:27:10 +0300

    this commit adds search template

--- insta/urls.py
+++ insta/urls.py
@@ -7,6 +7,7 @@ from django.conf.urls.static import static
 urlpatterns=[
     url(r'^$',views.timeline,name='Timeline'),
     url(r'^comment/(?P<pk>\d+)',views.Comment_Image,name='image-comment'),
+    url(r'^search/',views.search_results, name='search-results'),
     
     
 ]
